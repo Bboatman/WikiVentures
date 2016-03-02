23624e3203a0f2b4c717b536389adcff998648e0
Brooke Boatman <bboatman@macalester.edu>
2016-03-01 19:31:52 -0600

    Added unit testing and finished up with the rudimentary interface build up

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,24 @@
+import sys
+import unittest
+sys.path.append('./..') #Add the Wikiventures parent folder to the path
 from Page import *
 
-test = Page("sample")
-print(test.getUrl())
-print(test.getChildren())
+#test = Page("sample")
+#print(test.getUrl())
+#print(test.getChildren())
+
+# Documentation on testing!
+
+class TestPageMethods(unittest.TestCase):
+	def setUp(self):
+		self.test = Page('Sample')
+
+	def test_getUrl(self):
+		self.assertEqual(self.test.getUrl(), 'Sample')
+
+	def test_getChildren(self):
+		self.assertEqual(self.test.getChildren(), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
