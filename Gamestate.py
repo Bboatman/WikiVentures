1fbbcc4f7d223891bbd1b8c73afcd628d8d45c65
Brooke Boatman <bboatman@macalester.edu>
2016-03-03 17:25:33 -0600

    Pulled out some of the logic from the main method and made things a little more legible

--- Gamestate.py
+++ Gamestate.py
@@ -2,17 +2,21 @@
 from WikiArea import *
 
 class Gamestate(object):
-	def __init__(self, source = 'Picked Cucumber', target = 'Jesus'):
+	def __init__(self, source = 'Picked Cucumber', target = 'Jesus', debugState = False):
 		self.source = source
 		self.target = target
 		self.currentArea = WikiArea(source)
 		self.path = [source]
+		self.debugState = debugState
 
 	def isWin(self):
 		# Check if this page is a win
 		if self.path[-1] == self.target:
 			return True
 		return False
+
+	def isDebug(self):
+		return self.debugState
 
 	def getPath(self):
 		return self.path
@@ -22,13 +26,10 @@ class Gamestate(object):
 		return self.path[-1]
 
 	def getNumClicks(self):
-		return len(self.path)
+		return len(self.path) - 1
 
 	def getTarget(self):
 		return self.target
-
-	def winGame(self):
-		self.win = True
 
 	def addToPath(self, destination):
 		possibleDestinations = self.currentArea.getChildren()
