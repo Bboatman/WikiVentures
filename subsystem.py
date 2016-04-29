25bb7644a483ff2e3094e5ac9e4417306c40c16f
Waabanang <jhermes@macalester.edu>
2016-04-29 13:42:28 -0500

    Slowed the ship back down, and made planets randomly display

--- subsystem.py
+++ subsystem.py
@@ -1,17 +1,19 @@
 from body import Body, Star
 from kivy.core.window import Window
+from random import shuffle
 
 class SubSystem(object):
     def __init__(self, planet_names, title):
         self.title = title
         self.star = Star(title)
         #self.centerSystem()
+        shuffle(planet_names)
         self.planets = []
         i = 1
         for name in planet_names:
             self.planets.append(Body(name, i))
             i += 1
-
+        
 
     def update(self, dt):
         for planet in self.planets:
